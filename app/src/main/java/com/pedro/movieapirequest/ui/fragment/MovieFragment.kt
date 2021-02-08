d5fd3b80d13dd3804400c05903353b9f0cc5d28c
PedroHenriqueAlmeida <57142761+PedroHenriqueAlmeida@users.noreply.github.com>
2021-02-07 21:33:29 -0300

    MovieFragment implementation. ViewModel used to control the view state, and provide data to be used on the fragment.
    Most of the code went to the MovieFragment, where it'll show the current status of the movie request, if the network request went good or not. Based on that, when the response return essential informations about the movies, the fragment will show data on a layout structure based on TodoMovies4 movie screen. The fragment has a recyclerView that'll show data from similar movies by the SimilarMoviesAdapter.

--- app/src/main/java/com/pedro/movieapirequest/ui/fragment/MovieFragment.kt
+++ app/src/main/java/com/pedro/movieapirequest/ui/fragment/MovieFragment.kt
@@ -6,11 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.pedro.movieapirequest.R
 import com.pedro.movieapirequest.databinding.FragmentMovieBinding
+import com.pedro.movieapirequest.models.movie.Movie
+import com.pedro.movieapirequest.models.movie.Movies
+import com.pedro.movieapirequest.models.resources.Resources
+import com.pedro.movieapirequest.models.similarmovie.SimilarMovie
 import com.pedro.movieapirequest.ui.activity.MovieActivity
+import com.pedro.movieapirequest.ui.adapter.SimilarMovieAdapter
 import com.pedro.movieapirequest.ui.viewmodel.MovieViewModel
+import com.pedro.movieapirequest.utils.ApiUtils
 
 
 class MovieFragment : Fragment() {
@@ -18,10 +27,21 @@ class MovieFragment : Fragment() {
     private lateinit var binding: FragmentMovieBinding
     private lateinit var viewModel: MovieViewModel
 
+    companion object {
+
+        fun newInstance(): MovieFragment {
+
+            val fragment = MovieFragment()
+            return fragment
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie, container, false)
 
         val viewModelFactory =
             ViewModelProvider.AndroidViewModelFactory((activity as MovieActivity).application)
@@ -31,16 +51,88 @@ class MovieFragment : Fragment() {
             viewModelFactory
         ).get(MovieViewModel::class.java)
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie, container, false)
+        viewModel.getMoviesLiveData().observe(viewLifecycleOwner, Observer {
+            fillLayoutByResources(it)
+        })
+
+        viewModel.searchMovies()
+
+        binding.apply {
+            btnTentarNovamente.setOnClickListener({
+                viewModel.searchMovies()
+            })
+        }
+
         return binding.root
     }
 
-    companion object {
+    private fun loadSimilarMovieAdapter(similarMovies: List<SimilarMovie>) {
 
-        fun newInstance(): MovieFragment {
-
-            val fragment = MovieFragment()
-            return fragment
+        binding.similarMoviesRecyclerView.apply {
+            adapter = SimilarMovieAdapter(context, similarMovies)
+            layoutManager = LinearLayoutManager(activity)
         }
+    }
+
+    private fun loadMovieData(movie: Movie) {
+
+        binding.apply {
+            this.movie = movie
+
+            numberOfVisualization.text = movie.popularity.toString()
+
+            btnLike.setOnClickListener({
+                if (btnLike.isChecked) {
+                    movie.vote += 1
+                } else {
+                    movie.vote -= 1
+                }
+            })
+        }
+
+        Glide.with(this)
+            .load(ApiUtils.getImageUrl(movie.poster_path))
+            .centerCrop()
+            .into(binding.movieImage)
+    }
+
+    private fun fillLayoutByResources(movies: Resources<Movies>) {
+
+        var errorIconVisiblity = View.GONE
+        var movieLayoutVisibility = View.GONE
+        var progressBarVisibility = View.GONE
+        var movieStatusLayoutVisibility = View.GONE
+        var btnTentarNovamenteVisibility = View.GONE
+        var statusMessage = ""
+
+        when (movies) {
+            is Resources.Success -> {
+                movies.data?.movie?.let {
+                    loadMovieData(it)
+                }
+                movies.data?.similarMovies?.let {
+                    loadSimilarMovieAdapter(it)
+                }
+                movieLayoutVisibility = View.VISIBLE
+            }
+            is Resources.Error -> {
+                errorIconVisiblity = View.VISIBLE
+                movieStatusLayoutVisibility = View.VISIBLE
+                btnTentarNovamenteVisibility = View.VISIBLE
+                statusMessage = movies.message.toString()
+            }
+            is Resources.Loading -> {
+                progressBarVisibility = View.VISIBLE
+                movieStatusLayoutVisibility = View.VISIBLE
+                statusMessage = context?.getString(R.string.aguarde_buscando_filme).toString()
+            }
+        }
+
+        binding.movieStatusText.text = statusMessage
+        binding.errorIcon.visibility = errorIconVisiblity
+        binding.movieLayout.visibility = movieLayoutVisibility
+        binding.progressBar.visibility = progressBarVisibility
+        binding.movieStatusLayout.visibility = movieStatusLayoutVisibility
+        binding.btnTentarNovamente.visibility = btnTentarNovamenteVisibility
     }
 }
