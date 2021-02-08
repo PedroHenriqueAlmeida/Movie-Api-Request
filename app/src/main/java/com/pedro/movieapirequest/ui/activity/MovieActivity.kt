16f64033ff14cceba31eda57808d31bb486c571d
PedroHenriqueAlmeida <57142761+PedroHenriqueAlmeida@users.noreply.github.com>
2021-02-07 21:15:44 -0300

    MovieActivity implementation calling MovieFragment. Application Theme was changed to don't use ActionBar, and primary and secondary colors was changed
    The MovieActivity is finished and it's calling the MovieFragment, that'll hold most of the screen features

--- app/src/main/java/com/pedro/movieapirequest/ui/activity/MovieActivity.kt
+++ app/src/main/java/com/pedro/movieapirequest/ui/activity/MovieActivity.kt
@@ -3,9 +3,11 @@ package com.pedro.movieapirequest.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.pedro.movieapirequest.R
 import com.pedro.movieapirequest.databinding.ActivityMovieBinding
+import com.pedro.movieapirequest.ui.fragment.MovieFragment
 import com.pedro.movieapirequest.ui.viewmodel.MovieViewModel
 
 class MovieActivity : AppCompatActivity() {
@@ -17,10 +19,19 @@ class MovieActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
+
         val viewModelFactory = ViewModelProvider.AndroidViewModelFactory.getInstance(application)
 
         viewModel = ViewModelProvider(this, viewModelFactory).get(MovieViewModel::class.java)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
+        initFragment(MovieFragment.newInstance())
+    }
+
+    private fun initFragment(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction()
+            .replace(binding.frameLayout.id, fragment)
+            .commit()
     }
 }
